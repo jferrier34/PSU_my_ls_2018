54a4d72c232c734640104c6d517cbbd235048654
Julien Ferrier <jferrier@localhost.localdomain>
2018-11-13 16:01:55 +0100

    [modify] the function ls-l work whith correct format of the date thanks to new structure tm to get all the infos that I want. Missing the total in this function

--- my_ls.c
+++ my_ls.c
@@ -1,5 +1,4 @@
-/*
-** EPITECH PROJECT, 2018
+/* EPITECH PROJECT, 2018
 ** info.c
 ** File description:
 **
@@ -18,9 +17,42 @@
 
 #define couleur(param) fprintf(stderr, "\b \033[%sm", param)
 
+int my_strlen(char* str)
+{
+	int len;
+
+	len = 0;
+	while(str[len] != '\0')
+	{
+		len = len + 1;
+	}
+	return (len);
+}
+
+char *parse_time(char *time, char *src)
+{
+    int i = 0;
+    int size_time = my_strlen(time);
+
+    while (src[i] <= size_time) {
+        if (src[i] == 9) {
+            time[0] = src[i];
+            i++;
+        }
+    }
+    return (time);
+}
+
 void display(struct dirent *dir, char *repertory, struct stat *info)
 {
+    struct stat heure;
+    char buff[20];
+    struct tm * timeinfo;
+    stat(repertory, &heure);
+    timeinfo = localtime (&(heure.st_mtime));
     int cpt = stat(repertory, info);
+    // char *time = ctime(&info->st_mtime);
+    //my_put_nbr(my_strlen(time));
     struct passwd *pwd = getpwuid(info->st_uid);
     struct group *group = getgrgid(info->st_gid);
 
@@ -44,6 +76,8 @@ void display(struct dirent *dir, char *repertory, struct stat *info)
     my_putchar(' ');
     my_put_nbr(info->st_size);
     my_putchar(' ');
+    strftime(buff, 25, "%d %b. %H:%M ", timeinfo);
+    my_putstr(buff);
     my_putstr(dir->d_name);
     my_putchar(' ');
     my_putchar('\n');
@@ -59,6 +93,7 @@ int ls_l(void)
     while (readfiles = readdir(repertory))
     if (readfiles->d_name[0] != '.')
         display(readfiles, readfiles->d_name,&info);
+
     return (0);
 }
 
